da80edc271ca36fa7cddc42ef38800621daa5b9b
jonah <jonah.n.rubin@gmail.com>
2020-10-22 13:20:13 -0400

    CA world is a torus

--- CA/scripts/build.js
+++ CA/scripts/build.js
@@ -10,7 +10,7 @@ let iters = 0;
 let chancePredator = 0.15;
 let reductionRate = 0.7;
 
-let spec0deathRate = 0.000005;
+let spec0deathRate = 0.000000005;
 let spec0reproductionRate = 0.35;
 let spec0speed = 6;
 let spec0StarveTime = 550;
@@ -18,18 +18,21 @@ let spec0FOV = screen.width/10;
 
 let spec1reproductionRate = 0.95;
 let spec1speed = 8;
-let spec1deathRate = 0.000001;
+let spec1deathRate = 0.000000001;
 let spec1StarveTime = 900;
 let spec1FOV = screen.width/7;
 
-
 let foodProductionRate = 0.8;
 
-let ypop = 33;
+let ypop = 32;
 let xpop = 59;
 
 let totalPop = ypop * xpop;
 let slider;
+let chanceAgent;
+
+const mod = (x, n) => (x % n + n) % n;
+
 
 
 setup = function() {
@@ -44,7 +47,6 @@ setup = function() {
 
 function start() {
     let chanceAgent = numAgents/totalPop;
-
     agents = [];
     background(5);
     world.generate();
@@ -80,10 +82,72 @@ function newSpec1() {
 draw = function() {
 
     // reset the screen every draw loop
-    background(5);
-
+    // background(5);
     iters++;
     world.display();
+    updateGrid();
+
+};
+
+updateGrid = function() {
+
+    let newAgents = [];
+    let newRows = [];
+    let currAgent;
+    let move;
+    let newX, newY;
+    let ind;
+
+    for (let r = 0; r < ypop; ++r) {
+        newRows = [];
+        for (let c = 0; c < xpop; ++c) {
+            newRows.push(null);
+        }
+        newAgents.push(newRows);
+        newRows = [];
+    }
 
 
+    // for (let r = 0; r < ypop; ++r) {
+    //     for (let c = 0; c < xpop; ++c) {
+    for (let r = ypop-1; r >= 0; --r) {
+        for (let c = xpop-1; c >= 0; --c) {
+            currAgent = agents[r][c];
+            if (currAgent !== null) {
+
+                ind = Math.floor(Math.random() * currAgent.movesLeft.length);
+                move = currAgent.movesLeft[ind];
+
+                // // move is on the board
+                // while (r+move[0] < 0 || r+move[0] >= ypop || c+move[1] < 0 || c+move[1] >= xpop) {
+                //     // need to make sure place is empty too, remove possible new locations until none left
+                //     currAgent.movesLeft.splice(ind, 1);
+                //     ind = Math.floor(Math.random() * currAgent.movesLeft.length);
+                //     move = currAgent.movesLeft[ind];
+                // }
+
+                // if predator
+                if (currAgent.species === 0) {
+
+                }
+                newX = mod(r + move[0], ypop);
+                newY = mod(c + move[1], xpop);
+
+                // if (agents[newX][newY] == null && agents[newX][newY] == null) {
+                //     agents[newX][newY] = currAgent;
+                // } else {
+                //     agents[r][c] = currAgent;
+                // }
+
+                if (newAgents[newX][newY] == null && agents[newX][newY] == null) {
+                    newAgents[newX][newY] = currAgent;
+                } else {
+                    newAgents[r][c] = currAgent;
+                }
+
+                currAgent.movesLeft = currAgent.validMoves;
+            }
+        }
+    }
+    agents = newAgents;
 };
--- CA/scripts/menu.js
+++ CA/scripts/menu.js
@@ -18,7 +18,7 @@ function Menu() {
         window.location.href = "../index.html";
     }
 
-    let numAgentsSlider = createSlider(0, 1000, numAgents);
+    let numAgentsSlider = createSlider(0, 1888, numAgents);
     numAgentsSlider.position(10, 75);
     numAgentsSlider.style('width', '80px');
     numAgentsSlider.mouseReleased(updateNumAgents);
@@ -30,24 +30,6 @@ function Menu() {
 
     function updateNumAgents() {
         numAgents = numAgentsSlider.value();
-    }
-
-    let predatorSpeedSlider = createSlider(0, 50, spec0speed);
-    predatorSpeedSlider.position(10, 50 + 25 * numGlobalSliders);
-    predatorSpeedSlider.style('width', '80px');
-    predatorSpeedSlider.mouseReleased(updatePredSpeed);
-
-    myDiv = createDiv('Speed');
-    myDiv.position(100, 48 + 25 * numGlobalSliders);
-    myDiv.style('font-size', '19px');
-    myDiv.style('color', predatorColor);
-
-    function updatePredSpeed() {
-        spec0speed = predatorSpeedSlider.value();
-        for (let i = 0; i < agents.length; i++) {
-            if (agents[i].species === 0)
-                agents[i].movementRange = predatorSpeedSlider.value();
-        }
     }
 
     let chancePredatorSlider = createSlider(0, 100, chancePredator * 100);
